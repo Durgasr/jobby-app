51b77751bbfc0b8242b50753caa3400c16a31d10
Durgasri <challadurgasri2000@gmail.com>
2024-06-23 04:58:57 +0530

    profile details changed

--- src/components/ProfileDetails/index.js
+++ src/components/ProfileDetails/index.js
@@ -18,8 +18,8 @@ const ProfileDetails = props => {
     return (
       <div className="profile-details-container">
         <img src={profileImageUrl} alt="profile" className="profile-image" />
-        <h1 className="profile-name">Santosh Kumar</h1>
-        <p className="profile-bio">{shortBio}</p>
+        <h1 className="profile-name">Durgasri Challa</h1>
+        <p className="profile-bio">Software Engineer</p>
       </div>
     )
   }
